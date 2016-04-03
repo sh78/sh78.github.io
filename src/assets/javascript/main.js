111257faa2cd1168d623d8cbebbfbde2d95600a2
Sean Henderson <mail@sean.sh>
2016-04-02 20:03:27 -0700

    link entire post preview card to article

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,2 +1,8 @@
 // http://materializecss.com/navbar.html
 $(".button-collapse").sideNav();
+
+// link entire card to post in posts index
+$('.post-list li').on('click', function() {
+  var the_link = $('.post-link', this).attr('href');
+  window.location = the_link;
+}).css('cursor', 'pointer')
