284cf83a647d19844db7879e3df228e41c120ef7
Sean Henderson <mail@sean.sh>
2016-04-12 00:44:01 -0700

    wrap main js in document.ready

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -1,8 +1,12 @@
-// http://materializecss.com/navbar.html
-$(".button-collapse").sideNav();
+$(function() {
 
-// link entire card to post in posts index
-$('.post-list li').on('click', function() {
-  var the_link = $('.post-link', this).attr('href');
-  window.location = the_link;
-}).css('cursor', 'pointer')
+  // http://materializecss.com/navbar.html
+  $(".button-collapse").sideNav();
+
+  // link entire card to post in posts index
+  $('.post-list li').on('click', function() {
+    var the_link = $('.post-link', this).attr('href');
+    window.location = the_link;
+  }).css('cursor', 'pointer')
+
+});
