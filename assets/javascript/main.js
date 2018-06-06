d9f386872d079d34eed442e0980cf6498e7ba996
Sean Henderson <mail@sean.sh>
2018-06-05 17:03:37 -0700

    scaffold 'more' dropdown

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,3 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init();
 });
+
+// activate M dropdowns
+$(".dropdown-trigger").dropdown();
