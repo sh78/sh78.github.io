7ed034790e24ea2bd78c65f10542a6c77d4ce679
Sean Henderson <mail@sean.sh>
2018-06-06 19:08:27 -0700

    automatic lazyness
    + implement slightly improved version of google developers' lazy loading
    technique
      https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video/#lazy_loading_images

--- _posts/2018-05-26-welcome-to-jekyll.markdown
+++ _posts/2018-05-26-welcome-to-jekyll.markdown
@@ -23,3 +23,34 @@ Check out the [Jekyll docs][jekyll-docs] for more info on how to get the most ou
 [jekyll-docs]: https://jekyllrb.com/docs/home
 [jekyll-gh]:   https://github.com/jekyll/jekyll
 [jekyll-talk]: https://talk.jekyllrb.com/
+
+<div>
+  Amet saepe quisquam rem voluptas ut dolores culpa et illum. Illo corporis similique enim at excepturi. Aliquid quidem recusandae debitis commodi quisquam! Ipsum omnis ullam tempora amet quo officiis Commodi
+</div>
+
+```javascript
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.srcset = lazyImage.dataset.srcset;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  } else {
+    // Possibly fall back to a more compatible method here
+  }
+});
+```
+
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,8 +1,70 @@
+/*jshint esversion: 6 */
+
 M.AutoInit();
 
 (function () {
   document.addEventListener('DOMContentLoaded', function() {
     // add class confirming dom is loaded
     document.querySelector('html.js').classList.add('loaded');
+
+    // lazy loading per
+    // https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/
+    var lazyImages = [].slice.call(document.querySelectorAll("img"));
+    for(let i = 0; i < lazyImages.length; i++) {
+      console.log(lazyImages[i]);
+    }
+
+    if ("IntersectionObserver" in window) {
+      let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            // try to use data, or fallback to original src
+            lazyImage.src = lazyImage.dataset.src || lazyImage.src;
+            // same here, meaning srcset is not strictly required
+            lazyImage.srcset = lazyImage.dataset.srcset || lazyImage.srcset;
+            lazyImage.classList.remove("lazy");
+            lazyImageObserver.unobserve(lazyImage);
+          }
+        });
+      });
+
+      lazyImages.forEach(function(lazyImage) {
+        lazyImageObserver.observe(lazyImage);
+      });
+    } else {
+      const lazyLoad = function() {
+        if (active === false) {
+          active = true;
+
+          setTimeout(function() {
+            lazyImages.forEach(function(lazyImage) {
+              if ((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== "none") {
+                lazyImage.src = lazyImage.dataset.src;
+                lazyImage.srcset = lazyImage.dataset.srcset;
+                lazyImage.classList.remove("lazy");
+
+                lazyImages = lazyImages.filter(function(image) {
+                  return image !== lazyImage;
+                });
+
+                if (lazyImages.length === 0) {
+                  document.removeEventListener("scroll", lazyLoad);
+                  window.removeEventListener("resize", lazyLoad);
+                  window.removeEventListener("orientationchange", lazyLoad);
+                }
+              }
+            });
+
+            active = false;
+          }, 200);
+        }
+      };
+      document.addEventListener("scroll", lazyLoad);
+      window.addEventListener("resize", lazyLoad);
+      window.addEventListener("orientationchange", lazyLoad);
+    }
+
+
   });
 })();
